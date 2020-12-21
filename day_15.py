1658d2269192ed4cfadc0c59f8e3f127f49c7294
George <george.vince@hotmail.co.uk>
2020-12-21 18:08:40 +0000

    added: day 15

---
+++ day_15.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+data = [0, 3, 1, 6, 7, 5]
+
+
+def memory_game(size):
+    seen = {}
+
+    for i in range(len(data) - 1):
+        num = data[i]
+        seen[num] = i
+
+    for i in range(len(data) - 1, size - 1):
+        num = data[i]
+
+        if num not in seen:
+            data.append(0)
+            seen[num] = i
+
+        else:
+            last_seen = seen[num]
+            newNum = i - last_seen
+            data.append(newNum)
+            seen[num] = i
+
+    return data[-1]
+
+
+print("Part 1")
+print(memory_game(2020))
+print("Part 2")
+print(memory_game(30000000))
